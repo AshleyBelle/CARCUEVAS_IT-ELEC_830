a1d7c04390ed8676321311573e236805f541142d
Ashley Belle <ashleybelle780@gmail.com>
2023-02-11 13:18:53 +0800

    Ashley Belle Carcuevas

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.helloandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG,  "This is verbose log.");
+        Log.d(TAG, "This is a debug log.");
+        Log.i(TAG, "This is an info log.");
+        Log.w(TAG, "This is a warn log.");
+        Log.e(TAG, "This is an error log.");
+
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+            }
+        });
+
+
     }
 }
