3b8e76c61d14d3ea74c291d1e3202379cd989f20
Admin <ashleybelle780@gmail.com>
2023-02-18 13:12:09 +0800

    CARCUEVAS ACT2 02/18/23

---
+++ app/src/main/java/com/example/helloandroid/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( SecondActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
