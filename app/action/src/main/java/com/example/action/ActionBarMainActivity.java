54b0b1ba8f254b0114ddb67c7323d72dd2417933
Admin <ashleybelle780@gmail.com>
2023-03-18 14:11:36 +0800

    03/18/23

--- app/action/src/main/java/com/example/action/ActionBarMainActivity.java
+++ app/action/src/main/java/com/example/action/ActionBarMainActivity.java
@@ -1,32 +1,76 @@
 package com.example.action;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.app.ActionBar;
+import androidx.appcompat.app.ActionBar;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ActionBarMainActivity extends AppCompatActivity {
 
-    @SuppressLint("ResourceType")
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_bar_main);
 
-        ActionBar actionBar = getActionBar();
+        ActionBar actionBar = getSupportActionBar();
 
         actionBar.setTitle(" Gfg | ActionBar");
 
         actionBar.setSubtitle("This is an action bar");
 
-        actionBar.setSubtitle(R.drawable.ic_launcher_background);
+        actionBar.setIcon(R.mipmap.ic_launcher);
 
         actionBar.setDisplayUseLogoEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
-    //@Override
-    //public boolean onOptionsItemSelected(Menu menu)
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.menu, menu);
+            return super.onCreateOptionsMenu(menu);
+    }
+        @Override
+        public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.search:
+                showAlertDialog();
+                break;
+            case R.id.refresh:
+                DialogFragment dialogFragment = new DialogFragment();
+                dialogFragment.show(getSupportFragmentManager(), "My Fragment");
+            case R.id.copy:
+                Toast.makeText(this,"copy clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+
+        }
+        private void showAlertDialog(){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Alert!")
+                    .setMessage("Danger your falling");
+            builder.setPositiveButton("I know", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+        }
+
+
+
 
 }
