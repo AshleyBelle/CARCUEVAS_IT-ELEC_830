19f028eac9a482a1b723109ab16ac8a39c3c8c53
Adrean <ashleybelle780@gmail.com>
2023-04-15 12:56:00 +0800

    04/15/23

---
+++ buttomnav/src/main/java/com/example/buttomnav/MainActivity2.java
@@ -0,0 +1,23 @@
+package com.example.buttomnav;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        TextView textView = (TextView) findViewById(R.id.text_second);
+
+        Intent intent = getIntent();
+        String fromMain = intent.getStringExtra("PASS_ME");
+
+        textView.setText(fromMain);
+    }
+}
