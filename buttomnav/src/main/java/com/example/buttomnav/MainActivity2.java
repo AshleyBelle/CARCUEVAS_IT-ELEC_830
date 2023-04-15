f88c7580d0408453efc8de8852d1591bc4700665
Adrean <ashleybelle780@gmail.com>
2023-04-15 13:25:36 +0800

    04/15/23

--- buttomnav/src/main/java/com/example/buttomnav/MainActivity2.java
+++ buttomnav/src/main/java/com/example/buttomnav/MainActivity2.java
@@ -6,7 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.buttomnav.databinding.ActivityMainBinding;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
